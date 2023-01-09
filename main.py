cbdaa428be5c15296ed2ad5a80a80c53ddac3b3b
AidenBeresford <aidenberes@gmail.com>
2023-01-08 22:59:53 -0600

    Update main.py
    Added a toggle for case sensitivity

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import sys
-
 import scraper
 
 url = "https://github.com"
 phrase = "Did you forget to enter a phrase?"
+sensitive = True
 
 while True:
-    print("\nChoose an option:\n1. Enter URL\n2. Enter Phrase\n3. Search Website\n4. Quit\n")
+    print("\nChoose an option:\n1. Enter URL\n2. Enter Phrase\n3. Search Website\n4. Toggle"
+          " Case Sensitivity (" + str(sensitive) + ")\n5. Quit\n")
     choice = input("Enter number: ")
 
     match choice:
@@ -14,13 +15,22 @@ while True:
             url = input("URL: ")
 
         case '2':
-            phrase = input("Phrase (Case Sensitive): ")
+            if sensitive:
+                phrase = input("Phrase (Case Sensitive): ")
+            else:
+                phrase = input("Phrase (Case Insensitive): ")
 
         case '3':
-            scraper.scrape(url, phrase)
+            scraper.scrape(url, phrase, sensitive)
             # function used in scraper.py
 
         case '4':
+            if sensitive:
+                sensitive = False
+            else:
+                sensitive = True
+
+        case '5':
             sys.exit()
             # exits the program
 
